16b091a3da65e5cdcaa694cce8e88a8baf6200d7
Jorge Luis Boeira Bavaresco <jorge@jorge.ifsul>
2024-09-17 16:22:42 -0300

    home com os produtos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,39 @@
-export default function Home() {
+import { getProdutosDB } from '@/bd/useCases/produtoUseCases';
+import CardProduto from '@/componentes/comuns/CardProduto';
+
+export const revalidate = 60; // revalida a cada 30 segundos
+
+export default async function Home() {
+
+  const produtos = await getProdutosDB();
   return (
-    <div>
-       <h1>Home</h1>
+    <div style={{ padding: '20px' }}>
+      <div className="row">
+
+        {produtos.length > 0 && (
+
+          produtos.map(objeto => (
+
+            <div className="col-sm-3" key={objeto.codigo}>
+              <div className="card mb-3 text-center">
+                <div className="card-header">
+                  {objeto.nome}
+                </div>
+                <div className="card-body ">
+                  <h5 className="card-title">{objeto.nome}</h5>
+                  <p className="card-text">{objeto.descricao}</p>
+                  <p className="card-text"><small className="text-muted">Preço: {objeto.valor}</small></p>
+                  <p className="card-text"><small className="text-muted">Categoria: {objeto.categoria_nome}</small></p>
+                  <p className="card-text"><small className="text-muted">Estoque: {objeto.quantidade_estoque}</small></p>
+                </div>
+                <div class="card-footer text-muted">
+                  <button type="button" className="btn" >Detalhes do produto</button>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
