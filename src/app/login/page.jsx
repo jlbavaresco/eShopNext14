5b0232a6274c8d79666ebcc7f4f2ee86cd9d27d7
Jorge Luis Boeira Bavaresco <jorge@jorge.ifsul>
2024-10-02 17:19:06 -0300

    ajustes na tela de login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,14 +3,15 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { signIn } from "next-auth/react";
 
-export default async function Login() {
+export default async function Login({ searchParams }) {
 
   const handleLogin = async (formData) => {
     console.log('formData: ' + JSON.stringify(formData));
-    await signIn("credentials", { 
-      email: formData.get('email'), 
-      senha: formData.get('senha'), 
-      callbackUrl: '/' });
+    await signIn("credentials", {
+      email: formData.get('email'),
+      senha: formData.get('senha'),
+      callbackUrl: '/'
+    });
   }
 
   return (
@@ -19,25 +20,30 @@ export default async function Login() {
         <div style={{ textAlign: 'center' }}>
           <h2>Login de Usuário</h2>
         </div>
-
-        <div className="col-12 col-md-6">        
+        {searchParams.error && (
+          <h4 className="text-center "
+            style={{ color: 'red' }}>
+            Falha ao efetuar o login. Usuário ou senha inválidos.
+          </h4>
+        )}
+        <div className="col-12 col-md-6">
           <Form action={handleLogin} method='POST'>
             <Form.Group className="mb-3" controlId="txtEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Informe o email" 
-              name="email" required/>
+              <Form.Control type="email" placeholder="Informe o email"
+                name="email" required />
             </Form.Group>
             <Form.Group className="mb-3" controlId="txtSenha">
               <Form.Label>Senha</Form.Label>
-              <Form.Control type="password" placeholder="Senha" 
-              name="senha" required/>
+              <Form.Control type="password" placeholder="Senha"
+                name="senha" required />
             </Form.Group>
             <div className="form-group text-center mt-3">
               <Button variant="primary" type="submit">
                 Efetuar Login
               </Button>
             </div>
-          </Form>          
+          </Form>
         </div>
       </div>
     </div>
